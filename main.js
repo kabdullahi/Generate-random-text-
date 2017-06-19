2a67763fb29523be86090d0f512f54a476663201
kabdullahi <kzabdull@gmail.com>
2017-06-19 18:12:10 -0400

    resummit

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ const express = require ("express");
 const app = express();
 
 
-var loremIpsum = require('lorem-ipsum')
-  , output     = loremIpsum();
+var loremIpsum = require('lorem-ipsum');
 
-app.use(express.static('public'));
+
+var output = loremIpsum({
+  count:3
+  ,units: 'paragraphs'
+  ,sentenceLowerBound: 5
+  ,sentenceUpperBound: 15
+  ,paragraphLowerBound: 3
+  ,paragraphUpperBound: 7
+  ,format: 'html'
+});
+
+
 
 app.get("/lorem", function(req, res) {
 
-  output = loremIpsum({
-    count: 1,
-    units: 'sentences',
-    sentenceLowerBound: 5,
-    sentenceUpperBound: 15,
-    paragraphLowerBound: 3,
-    paragraphUpperBound: 7,
-    format: 'html',
-  });
 
   res.send(output);
 });
